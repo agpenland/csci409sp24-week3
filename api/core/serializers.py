eb6a9121342ccb97229606010a2c41e94b8efb13
agpenland <agpenland@coastal.edu>
2024-02-14 17:24:40 -0500

    Added Runway Serializer

--- api/core/serializers.py
+++ api/core/serializers.py
@@ -5,4 +5,11 @@ class AirlineSerializer(serializers.ModelSerializer):
     class Meta:
         model = Airline
         fields = ['name', 'airline_code']
+
+
+class RunwaySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Runway
+        fields = ['airport', 'runway_number', 'runway_designation', 'length', 'width']
+        #read_only_fields = ('id',)
         
